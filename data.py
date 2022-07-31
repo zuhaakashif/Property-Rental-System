0aac9954e927df9a2f4c9e4a767eca0a474751f7
zuha <100795162+zuhaakashif@users.noreply.github.com>
2022-07-31 15:45:54 +0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,8 @@
+ticket_details = ["Muhammad Faisal", "Muhammad Taufeeq", "0315-7607184", "0313-23456543", "Villa", "Apartment", "Single Villa", "Deluxe Apartment - 2-room", "143", "321", "1000Rs", "2340Rs", "13800Rs","14500Rs", "17900Rs", "21300Rs"]
+
+customer_info_list = []                                                          #list to store the customer information
+rec_list = []                                                                    #list to store property recommendations
+duration_and_facilities_list = []                                                #list to store the duration of stay and facilites
+add_costs = []                                                                   #list to store additional costs
+normal_bill = []                                                                 #list to store the bill without discount/additional costs
+budget_and_guest_list = []
